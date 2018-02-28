b4d2fbde5ca51667bd3b77e6d88df7cd6126fa1a
euro <phuchit@omise.co>
2018-02-28 09:42:34 +0700

    first commit

---
+++ simple-library/src/main/java/me/ripzery/simplelibrary/SimpleHello.kt
@@ -0,0 +1,15 @@
+package me.ripzery.simplelibrary
+
+
+/**
+ * OmiseGO
+ *
+ * Created by Phuchit Sirimongkolsathien on 28/2/2018 AD.
+ * Copyright © 2017 OmiseGO. All rights reserved.
+ */
+
+class SimpleHello {
+    fun sayHi() {
+
+    }
+}
